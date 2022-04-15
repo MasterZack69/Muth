0e95df984cfe5144a5a13aa0798b98c329cbea1e
Zackie <102979057+MasterZack69@users.noreply.github.com>
2022-04-15 14:34:24 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,18 @@
 #include <iostream>
 #include "Muth.hpp"
 
-int main()
-{
-	Add(7, 8.7);
-	Sub(9, 8);
-	Multi(3, 3);
-	Div(6, 3);
-	Percent(2, 5);
-	PercentOf(1.5, 108);
-	std::cout << "\n";
-	AreaTri(6, 9);
-	AreaSq(6);
-	AreaRec(7, 9);
-	AreaTrap(4, 5, 2);
-	AreaRhom(4,7);
-	AreaPara(3,8);
-	std::cout << "\n";
-	VolCube(8);
-	VolCuboid(2,3,4);
-	VolRightCyl(8,9);
-
+int main() {
+	jod(7,8);
+	ghata(8,4);
+	guna(5,6);
+	bhag(7,5);
+	pratishat(6,25);
+	ka_pratishat(7,5);
+	trikon_ka_shetrafal(4,6);
+	chaturbhurj_ka_shetrafal(8,9);
+   samlambchaturbhurj_ka_shetrafal(7,8,10);
+   samchaturbhurj_ka_shetrafal(9,6);
+   samantar_chaturbhurj_ka_shetrafal(6,5);
+  belan_ka_aytan(6,3);
+  vritt_ka_shetrafal(4);
 }
