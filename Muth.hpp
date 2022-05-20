78fd36a7d5f8a1a3273172af0ca88e2f2fe62b19
Zackie <102979057+MasterZack69@users.noreply.github.com>
2022-05-20 10:50:19 +0530

    Add files via upload

---
+++ Muth.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+void jod(float a, float b);
+void ghata(float a, float b);
+void guna(float a, float b);
+void bhag(float a, float b);
+void pratishat(float a, float b);
+void ka_pratishat(float a, float b);
+void trikon_ka_shetrafal(float a, float b);
+void varg_ka_shetrafal(float a);
+void aayat_ka_shetrafal(float a, float b);
+void samlambchaturbhurj_ka_shetrafal(float a, float b, float c);
+void samchaturbhurj_ka_shetrafal(float a, float b);
+void samantar_chaturbhurj_ka_shetrafal(float a, float b);
+void ghan_ka_aytan(float a);
+void ghanabh_ka_aytan(float a, float b, float c);
+void belan_ka_aytan(float a, float b);
+void vritt_ka_shetrafal(float a);
+void varg_ki_paridhi(float a);
+void aayat_ka_parimap(float a, float b);
+void vritt_ki_paridhi(float a);
+void samtulya_tribhuj_ka_parimap(float a);
+void bal(float a, float b);
+void gatij_urja(float a, float b);
+void tvaran(float a, float b, float c);
+void dravyaman(float a, float b);
+void raftaar(float a, float b);
+void samay(float a, float b);
+void doori(float a, float b);
+void ghanatva(float a,float b);
